fe6caed5b3ea9c0c4dcf867195605f798d334677
Steven Kaufman <steveplaysguiola@gmail.com>
2019-05-01 16:00:07 +0000

    cin.cpp

--- 3/cin.cpp
+++ 3/cin.cpp
@@ -1,8 +1,24 @@
+//cin.cpp
+/*
+ * This program gives a brief overview of the cin command
+ */
+
 #include <iostream>
 
 using namespace std;
 
 int main(){
-
+    string name;
+    cout << "What's your name? ";
+    cin >> name;
+    cout << "Hello, " << name << "." << endl;
     return 0;
 }
+
+// Expected output: (user input wrapped in **)
+/*
+
+What's your name? *write name here*
+Hello, *name you wrote*.
+
+ */
