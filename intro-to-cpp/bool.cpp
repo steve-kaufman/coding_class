3afbbc3e840dfbc4188be555a654f6b609a52d69
Steven Kaufman <steveplaysguiola@gmail.com>
2019-05-05 19:24:09 +0000

    added bool.cpp

---
+++ intro-to-cpp/bool.cpp
@@ -0,0 +1,29 @@
+//boo.cpp
+/*
+ * Gives a brief overview of the bool data type
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    bool thisIsTrue = true;
+    bool thisIsFalse = false;
+    int trueInt = true;
+    int falseInt = false;
+
+    cout << thisIsTrue << endl;
+    cout << thisIsFalse << endl;
+    cout << trueInt << endl;
+    cout << falseInt << endl;
+    return 0;
+}
+
+//Expected output:
+/*
+    1
+    0
+    1
+    0
+*/
